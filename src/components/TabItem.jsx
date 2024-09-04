dc5fff5c9822102b751ccea279f1ee4194eaf61c
Dineshkumar-teki <dineshkumarteki497@gmail.com>
2024-09-04 19:44:14 +0530

    Assignment

--- src/components/TabItem.jsx
+++ src/components/TabItem.jsx
@@ -7,7 +7,7 @@ const TabItem = ({ tab, activeTab, setTab }) => {
         type="button"
         className={`${
           activeTab ? "text-white bg-[#363C43] shadow-3xl" : "text-[#A3ADB2]"
-        } font-medium text-[16px] h-12 rounded-2xl flex items-center px-8 outline-none lg:text-sm lg:px-3 xl:text-lg xl:px-7`}
+        } font-medium text-[16px] h-12 rounded-2xl flex items-center px-8 outline-none lg:text-sm lg:px-3 xl:text-lg xl:px-6`}
         onClick={() => {
           setTab(tab.id);
         }}
